ce0a3034c488146ea7d828f454775853c78ba32c
Ryan Chan <86031810+rkchan77@users.noreply.github.com>
2022-01-27 21:30:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -259,10 +259,18 @@ var json = [
 
   function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    var name = profile.getName();
+    var image = profile.getImageUrl();
+    var email = profile.getEmail();
+
+    for(var i=0; i<json.length; i++){
+        if(json[i].email == email){
+            document.body.style.backgroundColor = "green";
+            break;
+        } else {
+            document.body.style.backgroundColor = "red";
+        }
+    }
   }
 
   function signOut() {
@@ -270,14 +278,4 @@ var json = [
     auth2.signOut().then(function () {
       console.log('User signed out.');
     });
-  }
-
-  if (auth2.isSignedIn.get()) {
-    var profile = auth2.currentUser.get().getBasicProfile();
-    console.log('ID: ' + profile.getId());
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
   }
